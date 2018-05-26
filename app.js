ed3cbb5dd4d3d78ab3f90b90264894c3643facad
William Hendricks <wshendricks@gmail.com>
2018-05-25 22:31:38 -0500

    Think I successfully set up an endpoint...

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.get("/", function(req, res) {
+res.sendFile("/index.html", {"root": __dirname});
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
