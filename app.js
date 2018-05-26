6005925665a4a216bc90cd4e3de15e8d2488a412
William Hendricks <wshendricks@gmail.com>
2018-05-25 22:55:20 -0500

    Installed Mongoose. Updating App.js per JB tutorial instructions. Leery.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var path = require("path");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/module4-week1");
+
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+});
+var User = mongoose.model("User", nameSchema);
 
 
 // viewed at http://localhost:3000
